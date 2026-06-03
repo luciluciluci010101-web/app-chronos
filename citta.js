784d67b6bf1ca155c8b6f93ccf09ea4a75fdb6ec
luciluciluci010101-web <luciluciluci010101@gmail.com>
2026-06-03 23:59:50 +0200

    Update citta.js

--- citta.js
+++ citta.js
@@ -1,21 +1,6 @@
 // DATABASE DELLE CITTÀ E DELLE CIVILTÀ
 var listaCitta = [
     {
-        nome: "Tebe Imperiale",
-        tipo: "citta",
-        anno: -1300,
-        coordinate: [25.6872, 32.6396],
-        testo: "Nel cuore del Nuovo Regno, Tebe diventa una megalopoli splendida. Oro e tributi confluiscono nei templi di Karnak, mentre i Re vengono sepolti nella Valle dei Re."
-    },
-    {
-        nome: "Fondazione di Roma",
-        tipo: "citta",
-        anno: -753,
-        coordinate: [41.9028, 12.4964],
-        testo: "Secondo la leggenda, Romolo fonda la città sul colle Palatino. È l'inizio di una civiltà che dominerà l'intero bacino del Mediterraneo."
-    }
-var listaCitta = [
-    {
         nome: "Uruk: La Prima Metropoli",
         tipo: "citta",
         anno: -3000,
@@ -48,7 +33,7 @@ var listaCitta = [
         tipo: "citta",
         anno: -3000,
         coordinate: [-10.8931, -77.5203],
-        testo: "Situata nella Valle di Supe in Perù, è la città più antica del continente americano. Sviluppa una complessa architettura monumentale con piramidi di pietra e piazze cerimoniali senza l'uso della ceramica."
+        testo: "Situata nella Valle di Supe in Perù, è la città più antica del continent americano. Sviluppa una complessa architettura monumentale con piramidi di pietra e piazze cerimoniali senza l'uso della ceramica."
     },
     {
         nome: "Tebe Imperiale",
