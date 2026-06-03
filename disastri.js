2b92628afc94111331e42a32edc58398ef4fdde6
luciluciluci010101-web <luciluciluci010101@gmail.com>
2026-06-03 23:06:08 +0200

    Update and rename eventi.js to disastri.js

---
+++ disastri.js
@@ -0,0 +1,9 @@
+var listaDisastri = [
+    {
+        nome: "Grande Eruzione di Thera (Santorini)",
+        tipo: "natura",
+        anno: -1600,
+        coordinate: [36.4166, 25.4324],
+        testo: "Una delle più grandi eruzioni vulcaniche della storia umana."
+    }
+];
