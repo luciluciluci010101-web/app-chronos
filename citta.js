e5a9e6286ea50c2e48afe5ddcdb10ea5304a7e17
luciluciluci010101-web <luciluciluci010101@gmail.com>
2026-06-03 23:54:14 +0200

    Update citta.js

--- citta.js
+++ citta.js
@@ -14,4 +14,54 @@ var listaCitta = [
         coordinate: [41.9028, 12.4964],
         testo: "Secondo la leggenda, Romolo fonda la città sul colle Palatino. È l'inizio di una civiltà che dominerà l'intero bacino del Mediterraneo."
     }
+var listaCitta = [
+    {
+        nome: "Uruk: La Prima Metropoli",
+        tipo: "citta",
+        anno: -3000,
+        coordinate: [31.3222, 45.6361],
+        testo: "In Mesopotamia (Iraq), Uruk è il più grande centro urbano del mondo antico. Protetta da mura monumentali, è la culla della scrittura cuneiforme e l'origine del mito di Gilgamesh."
+    },
+    {
+        nome: "Ur dei Sumeri",
+        tipo: "citta",
+        anno: -3000,
+        coordinate: [30.9632, 46.1031],
+        testo: "Fiorente città-stato sumera e potente porto fluviale sul fiume Eufrate. In questa epoca è un centro commerciale vitale che scambia merci con l'intera regione del Golfo."
+    },
+    {
+        nome: "Menfi: Capitale dei Faraoni",
+        tipo: "citta",
+        anno: -3000,
+        coordinate: [29.8444, 31.2509],
+        testo: "Fondata nel punto strategico in cui il Nilo si divide in delta, Menfi diventa la prima grande capitale dell'Egitto unificato, fulcro del potere politico e religioso del primo faraone Narmer."
+    },
+    {
+        nome: "Liangzhu: La Città d'Acqua",
+        tipo: "citta",
+        anno: -3000,
+        coordinate: [30.3958, 120.0039],
+        testo: "In Cina, questa avanzatissima cultura neolitica costruisce una città protetta da mura con un sistema idraulico di dighe, canali e bacini immenso, per controllare le alluvioni e coltivare il riso."
+    },
+    {
+        nome: "Caral: L'Alba delle Americhe",
+        tipo: "citta",
+        anno: -3000,
+        coordinate: [-10.8931, -77.5203],
+        testo: "Situata nella Valle di Supe in Perù, è la città più antica del continente americano. Sviluppa una complessa architettura monumentale con piramidi di pietra e piazze cerimoniali senza l'uso della ceramica."
+    },
+    {
+        nome: "Tebe Imperiale",
+        tipo: "citta",
+        anno: -1300,
+        coordinate: [25.6872, 32.6396],
+        testo: "Nel cuore del Nuovo Regno, Tebe diventa una megalopoli splendida. Oro e tributi confluiscono nei templi di Karnak, mentre i Re vengono sepolti nella Valle dei Re."
+    },
+    {
+        nome: "Fondazione di Roma",
+        tipo: "citta",
+        anno: -753,
+        coordinate: [41.9028, 12.4964],
+        testo: "Secondo la leggenda, Romolo fonda la città sul colle Palatino. È l'inizio di una civiltà che dominerà l'intero bacino del Mediterraneo."
+    }
 ];
