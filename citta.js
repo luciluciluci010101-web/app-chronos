7e9450dc10f15b4e6ace9c96e77013cd94d0b4ff
luciluciluci010101-web <luciluciluci010101@gmail.com>
2026-06-03 23:07:29 +0200

    Create citta.js

---
+++ citta.js
@@ -0,0 +1,17 @@
+// DATABASE DELLE CITTÀ E DELLE CIVILTÀ
+var listaCitta = [
+    {
+        nome: "Tebe Imperiale",
+        tipo: "citta",
+        anno: -1300,
+        coordinate: [25.6872, 32.6396],
+        testo: "Nel cuore del Nuovo Regno, Tebe diventa una megalopoli splendida. Oro e tributi confluiscono nei templi di Karnak, mentre i Re vengono sepolti nella Valle dei Re."
+    },
+    {
+        nome: "Fondazione di Roma",
+        tipo: "citta",
+        anno: -753,
+        coordinate: [41.9028, 12.4964],
+        testo: "Secondo la leggenda, Romolo fonda la città sul colle Palatino. È l'inizio di una civiltà che dominerà l'intero bacino del Mediterraneo."
+    }
+];
