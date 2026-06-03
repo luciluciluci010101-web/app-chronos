e851c146808e313beba1c3d3bb6eec54def0ac52
luciluciluci010101-web <luciluciluci010101@gmail.com>
2026-06-03 23:07:43 +0200

    Create guerre.js

---
+++ guerre.js
@@ -0,0 +1,17 @@
+// DATABASE DELLE GUERRE E DEI CONFLITTI
+var listaGuerre = [
+    {
+        nome: "Battaglia di Kadesh",
+        tipo: "guerra",
+        anno: -1274,
+        coordinate: [34.56, 36.52],
+        testo: "Il più grande scontro di carri della storia antica. Ramses II affronta l'Impero Ittita in una battaglia strategica che porterà al primo trattato di pace internazionale."
+    },
+    {
+        nome: "Costruzione della Grande Muraglia",
+        tipo: "guerra",
+        anno: -215,
+        coordinate: [40.4319, 116.5704],
+        testo: "L'imperatore Qin Shi Huang ordina di unificare le fortificazioni esistenti per difendere la Cina dalle incursioni dei popoli nomadi del nord."
+    }
+];
